739f87bb9e3c38afe69d37376a3ffbd409e90836
Hoang Quoc Phuong <qphwng2005@gmail.com>
2026-04-06 13:34:58 +0700

    Tạo các DTO chứa dữ liệu nhân viên
    - `NhanVienRequestDTO` chứa dữ liệu nhân viên gửi cho server để thực hiện
    ghi dữ liệu.
    - `NhanVienResponseDTO` chứa dữ liệu nhận được từ server gửi về.

---
+++ src/main/java/com/npmtt/ticketapi/dto/request/NhanVienRequestDTO.java
@@ -0,0 +1,18 @@
+package com.npmtt.ticketapi.dto.request;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class NhanVienRequestDTO {
+    private String maNhanVien;
+    private String matKhau;
+    private String hoTen;
+    private String ngaySinh;
+    private String gioiTinh;
+    private String sdt;
+    private String email;
+    private String diaChi;
+    private String vaiTro;
+}
---
+++ src/main/java/com/npmtt/ticketapi/dto/response/NhanVienResponseDTO.java
@@ -0,0 +1,15 @@
+package com.npmtt.ticketapi.dto.response;
+
+import lombok.Data;
+
+@Data
+public class NhanVienResponseDTO {
+    private String maNhanVien;
+    private String hoTen;
+    private String ngaySinh;
+    private String gioiTinh;
+    private String sdt;
+    private String email;
+    private String diaChi;
+    private String vaiTro;
+}
