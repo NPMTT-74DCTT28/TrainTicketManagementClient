a4502fc57cd216feb83fa0df2deb1841db37e3c8
Hoang Quoc Phuong <qphwng2005@gmail.com>
2026-04-06 15:19:27 +0700

    Thêm thư viện, tạo view, sửa DTO nhân viên
    - Thêm thư viện JCalendar.
    - Tạo thêm constructor tuỳ chỉnh cho `NhanVienRequestDTO`.
    - Thay đổi annotation cho `NhanVienRequestDTO`.
    - Thêm các panel, frame để hiển thị giao diện.

--- src/main/java/com/npmtt/ticketclient/dto/request/NhanVienRequestDTO.java
+++ src/main/java/com/npmtt/ticketclient/dto/request/NhanVienRequestDTO.java
@@ -1,9 +1,11 @@
 package com.npmtt.ticketclient.dto.request;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class NhanVienRequestDTO {
     private String maNhanVien;
@@ -15,4 +17,15 @@ public class NhanVienRequestDTO {
     private String email;
     private String diaChi;
     private String vaiTro;
+
+    public NhanVienRequestDTO(String maNhanVien, String hoTen, String ngaySinh, String gioiTinh, String sdt, String email, String diaChi, String vaiTro) {
+        this.maNhanVien = maNhanVien;
+        this.hoTen = hoTen;
+        this.ngaySinh = ngaySinh;
+        this.gioiTinh = gioiTinh;
+        this.sdt = sdt;
+        this.email = email;
+        this.diaChi = diaChi;
+        this.vaiTro = vaiTro;
+    }
 }
