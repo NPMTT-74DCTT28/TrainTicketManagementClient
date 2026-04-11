ee853f2273edfe04901122ac4958d1bdf923732c
Hoang Quoc Phuong <qphwng2005@gmail.com>
2026-04-11 18:03:39 +0700

    Tạo view và các DTO thống kê

---
+++ src/main/java/com/npmtt/ticketclient/dto/thongke/KhachHangVipDTO.java
@@ -0,0 +1,12 @@
+package com.npmtt.ticketclient.dto.thongke;
+
+import lombok.Data;
+
+@Data
+public class KhachHangVipDTO {
+
+    private String hoTen;
+    private String sdt;
+    private int soVeDaMua;
+    private double tongTienChiTieu;
+}
